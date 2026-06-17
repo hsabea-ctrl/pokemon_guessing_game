d52fc39b89711817bf3610a1c7eec49fc277ddeb
Sabea Hassan <hsabae497@gmail.com>
2026-06-17 13:35:29 -0400

    add

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import express from "express";
 const app = express (); 
 
 
+const id= 25;
+const game_id= Buffer.from(String(id)).toString("base64");
+
+
 app.get("/"
 , (req, res) => {
 res.send("GUESSING GAME!");
+res.send("Your game id is: $(game_id"); 
+
 });
 app.listen(3000);
 
