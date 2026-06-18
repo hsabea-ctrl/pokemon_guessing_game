683409841b70f9d4127f431e914afa14bfac33f5
Sabea Hassan <hsabae497@gmail.com>
2026-06-17 22:46:47 -0400

    id randomizer

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 import express from "express"; 
 const app = express (); 
 
-
-const id= 25;
-const game_id= Buffer.from(String(id)).toString("base64");
-
-
-app.get("/"
+app.get("/new"
 , (req, res) => {
-//res.send("GUESSING GAME!");
-res.send(`Your game id is: ${game_id}`); 
-
-});
+    const id=Math.floor(Math.random()*151)+1; //generate random id number
+    const game_id= Buffer.from(String(id)).toString("base64");//encode id into game_id 
+   
+    console.log(id);   //send response 
+     res.send(`
+        <h1>Pokemon Guessing Game</h1>
+        <p>Your game id is: ${game_id} </p>
+        <p>You have 5 hints for this game.</p>
+   `);
+    }); 
 app.listen(3000);
 
 
